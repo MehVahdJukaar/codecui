4074353526a268ef345d504b143ac92b4ea45515
Alexandra-Myers <bia416cat@gmail.com>
2026-07-14 17:13:44 -0400

    oopsies

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("me.modmuss50.mod-publish-plugin") version "0.8.+" apply false
 }
 
-stonecutter active "26.1-common"
+stonecutter active "26.1-neoforge"
 
 tasks.register("runActiveClient") {
     group = "stonecutter"
